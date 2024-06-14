3d13d1916977da7340b3b26d956b566b9021b409
varshasingannagari <69510093+varshasingannagari@users.noreply.github.com>
2024-06-14 00:59:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["HTML", "CSS", "JS"],
+        details: {
+            address: "Bangalore",
+            city: "Bangalore"
+        }
+    };
+    console.log(member.skills);
+    console.log(member.details.address);
+}
